d6cc24874c92ae2ab7c973efc37b844d663da6b6
BirdeeHub <robindbor@gmail.com>
2023-06-25 04:44:02 -0700

    removed extra modules from package script

--- app/bashpackage.sh
+++ app/bashpackage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [ -f "./minesweeper.cfg" ] && mv ./minesweeper.cfg ../
 InstallDir="/usr/local/games"
-jpackage --name minesweeper --description "Minesweeper for Linux" --input ../app --dest ../ --main-jar minesweeper.jar --icon PackageIcons/MineSweeperIcon.png --add-modules java.desktop,java.logging,java.xml  --install-dir $InstallDir
+jpackage --name minesweeper --description "Minesweeper for Linux" --input ../app --dest ../ --main-jar minesweeper.jar --icon PackageIcons/MineSweeperIcon.png --add-modules java.desktop --install-dir $InstallDir
 [ -f "../minesweeper.cfg" ] && mv ../minesweeper.cfg .
