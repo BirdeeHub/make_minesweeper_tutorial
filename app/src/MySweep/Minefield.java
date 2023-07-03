0bacef0cfe2c60162113b7032de6e4d0fbd287e2
BirdeeHub <robindbor@gmail.com>
2023-07-03 04:44:44 -0700

    Made a subtraction happen less often

--- app/src/MySweep/Minefield.java
+++ app/src/MySweep/Minefield.java
@@ -16,7 +16,7 @@ class Minefield{//a data class for Grid. contains and manages all mutable game-s
     private final Timer currentTimeTimer = new Timer();
     private TimerTask timeTask = new TimerTask() {
         public void run() {
-            time = System.currentTimeMillis();//dont add a time format here. This gets saved to scores file
+            time = System.currentTimeMillis()-startTime;//dont add a time format here. This gets saved to scores file
         }//                                                                           It may not compare for highscore correctly after doing that
     };
     //----------Constructor---------------------------------
@@ -132,7 +132,7 @@ class Minefield{//a data class for Grid. contains and manages all mutable game-s
     }
     boolean isFirstClick(){return firstClick;}
 
-    long getTime(){return time-startTime;}//get time
+    long getTime(){return time;}//get time
 
     //GameOver & stop timer (which then cannot be started again)
     void setGameOver(){
