d4c886b6143c9b8f668d18cd43be26989c202565
BirdeeHub <robindbor@gmail.com>
2023-07-13 15:55:04 -0700

    Fixed a bug that was in Jarred

--- app/src/MySweep/MineSweeper.java
+++ app/src/MySweep/MineSweeper.java
@@ -55,7 +55,6 @@ class MineSweeper {
                     OvrightJarPro.command().add(scoresFromClassPath);
                     List<String> command = OvrightJarPro.command();
                     String OvrightJarCommand = String.join(" ", command);
-                    System.out.println(OvrightJarCommand);
              	    Runtime.getRuntime().exec(OvrightJarCommand);
            	    } catch (IOException e) {e.printStackTrace();}
             }
