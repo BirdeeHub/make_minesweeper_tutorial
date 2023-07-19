338357fa000e8079efaf7f70ceb187c6fdc3665f
BirdeeHub <robindbor@gmail.com>
2023-07-19 07:18:34 -0700

    removed and unnecessary thing from packager

--- app/Packaging/winpackage.bat
+++ app/Packaging/winpackage.bat
@@ -1,5 +1,5 @@
 IF EXIST "..\Minesweeper.cfg" (move ..\Minesweeper.cfg ..\..\)
-jlink --compress=2 --add-modules java.desktop,java.management,java.compiler,jdk.jartool --output ..\..\jre
+jlink --compress=2 --add-modules java.desktop,java.management,jdk.jartool --output ..\..\jre
 jpackage --name Minesweeper_3.0 --description "Minesweeper for Windows with Jar-ception (compiler and jar tool included)" --input ..\..\app --dest ..\..\ --main-jar minesweeper.jar --icon ..\src\MySweep\Icons\Minesweeper.ico --runtime-image ..\..\jre --win-dir-chooser --win-menu --win-shortcut --win-shortcut-prompt --win-per-user-install --app-version 1.0 --vendor Birdee
 del /s /q ..\..\jre\
 rmdir /s /q ..\..\jre\
