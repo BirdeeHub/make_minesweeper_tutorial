bb21e3318c569b5314c1fcf2f40dfc94fc8f2a3d
BirdeeHub <robindbor@gmail.com>
2023-07-19 16:59:15 -0700

    fixed something in package script

--- app/Packaging/winpackage.bat
+++ app/Packaging/winpackage.bat
@@ -1,6 +1,8 @@
-IF EXIST "..\Minesweeper.cfg" (move ..\Minesweeper.cfg ..\..\)
+IF EXIST "..\Minesweeper_3.0.cfg" (move ..\Minesweeper.cfg ..\..\)
+IF EXIST "..\Minesweeper_2.0.cfg" (move ..\Minesweeper.cfg ..\..\)
 jlink --compress=2 --add-modules java.desktop,java.management --output ..\..\jre
 jpackage --name Minesweeper_3.0 --description "Minesweeper for Windows with Jar-ception" --input ..\..\app --dest ..\..\ --main-jar minesweeper.jar --icon ..\src\MySweep\Icons\Minesweeper.ico --runtime-image ..\..\jre --win-dir-chooser --win-menu --win-shortcut --win-shortcut-prompt --win-per-user-install --app-version 1.0 --vendor Birdee
 del /s /q ..\..\jre\
 rmdir /s /q ..\..\jre\
-IF EXIST "..\..\Minesweeper.cfg" (move ..\..\Minesweeper.cfg ..\)
+IF EXIST "..\..\Minesweeper_3.0.cfg" (move ..\..\Minesweeper.cfg ..\)
+IF EXIST "..\..\Minesweeper_2.0.cfg" (move ..\..\Minesweeper.cfg ..\)
