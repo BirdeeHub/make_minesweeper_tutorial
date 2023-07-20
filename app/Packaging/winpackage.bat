3d9ea0d45f37a6dde8945fc27c6e531848680de8
BirdeeHub <robindbor@gmail.com>
2023-07-19 17:07:54 -0700

    fixed something in package script

--- app/Packaging/winpackage.bat
+++ app/Packaging/winpackage.bat
@@ -1,3 +1,9 @@
-IF EXIST "..\Minesweeper.cfg" (move ..\Minesweeper.cfg ..\..\)
+IF EXIST "..\Minesweeper_1.0.cfg" (move ..\Minesweeper_1.0.cfg ..\..\)
+IF EXIST "..\Minesweeper_2.0.cfg" (move ..\Minesweeper_2.0.cfg ..\..\)
+IF EXIST "..\Minesweeper_3.0.cfg" (move ..\Minesweeper_3.0.cfg ..\..\)
+IF EXIST "..\Minesweeper_4.0.cfg" (move ..\Minesweeper_4.0.cfg ..\..\)
 jpackage --name Minesweeper_1.0 --description "Minesweeper for Windows" --input ..\..\app --dest ..\..\ --main-jar minesweeper.jar --icon ..\src\MySweep\Icons\Minesweeper.ico --add-modules java.desktop --win-dir-chooser --win-menu --win-shortcut --win-shortcut-prompt --win-per-user-install --app-version 1.0 --vendor Birdee
-IF EXIST "..\..\Minesweeper.cfg" (move ..\..\Minesweeper.cfg ..\)
+IF EXIST "..\..\Minesweeper_1.0.cfg" (move ..\..\Minesweeper_1.0.cfg ..\)
+IF EXIST "..\..\Minesweeper_2.0.cfg" (move ..\..\Minesweeper_2.0.cfg ..\)
+IF EXIST "..\..\Minesweeper_3.0.cfg" (move ..\..\Minesweeper_3.0.cfg ..\)
+IF EXIST "..\..\Minesweeper_4.0.cfg" (move ..\..\Minesweeper_4.0.cfg ..\)
