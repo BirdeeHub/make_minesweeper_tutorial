a7750d82e4cea54baba89b34243332c5aa20c233
BirdeeHub <robindbor@gmail.com>
2023-07-22 16:13:12 -0700

    small error message changes

--- app/src/OverwriteMinesweeperJar.java
+++ app/src/OverwriteMinesweeperJar.java
@@ -21,38 +21,33 @@ class OverwriteMinesweeperJar {
         File thisFile = new File(args[2]);
         File scoresFile = new File(args[3]);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            thisFile.delete();
+            System.out.println((thisFile.delete())?thisFile.toString()+" failed to delete!\n":"");
         }));
         boolean copySucceeded = false;
-        try(Scanner in = new Scanner(scoresFile)) {
+        try(Scanner in = new Scanner(scoresFile)){
             StringBuilder scoresFileStringBuilder = new StringBuilder();
-            while (in.hasNextLine()) {
-                scoresFileStringBuilder.append(in.nextLine());
-                if(in.hasNextLine())scoresFileStringBuilder.append('\n');
-            }
+            while(in.hasNextLine())scoresFileStringBuilder.append(in.nextLine()+((in.hasNextLine())?'\n':""));
             try{
                 writeJarWithNewScores(originalJarPath, scoresEntryName, scoresFileStringBuilder.toString());
                 copySucceeded = true;
-            }catch(IOException e){e.printStackTrace();
-                System.out.println("Overwrite failed: Move game file to a writeable directory. \n Then open and close game to complete save."); 
-            }
+            }catch(IOException e){System.out.println(e.getStackTrace().toString() + "\n Unable to overwrite"+originalJarPath+"! \n please move your game folder to a writeable directory");}
         }catch(FileNotFoundException e){}
         if(copySucceeded)scoresFile.delete();//<-- need to delete after closing Scanner otherwise it wont delete
     }
     private static void writeJarWithNewScores(String jarFilePath, String scoresEntryName, String newScoresFileContents) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try (JarInputStream jis = new JarInputStream(new FileInputStream(jarFilePath));
-            JarOutputStream jos = new JarOutputStream(baos, jis.getManifest())) {
+            JarOutputStream jos = new JarOutputStream(baos, jis.getManifest())){
             JarEntry entry;
-            while ((entry = jis.getNextJarEntry()) != null) {
-                if (!newScoresFileContents.equals(null)&&entry.getName().equals(scoresEntryName)) {
+            while((entry = jis.getNextJarEntry()) != null){
+                if(!newScoresFileContents.equals(null)&&entry.getName().equals(scoresEntryName)){
                     jos.putNextEntry(new JarEntry(scoresEntryName));
                     jos.write(newScoresFileContents.getBytes());
-                } else {
+                }else{
                     jos.putNextEntry(entry);
                     byte[] buffer = new byte[4096];
                     int bytesRead;
-                    while ((bytesRead = jis.read(buffer)) != -1) {
+                    while ((bytesRead = jis.read(buffer)) != -1){
                         jos.write(buffer, 0, bytesRead);
                     }
                 }
@@ -60,8 +55,6 @@ class OverwriteMinesweeperJar {
                 jos.closeEntry();
             }
         }
-        try (OutputStream outputStream = new FileOutputStream(jarFilePath)) {
-            baos.writeTo(outputStream);
-        }
+        try (OutputStream outputStream = new FileOutputStream(jarFilePath)){baos.writeTo(outputStream);}
     }
 }
