b9045f90a8d0dd4fb68e037ebd9dd62064350c7b
BirdeeHub <robindbor@gmail.com>
2023-07-22 09:22:47 -0700

    Improved how overwrite is called

--- app/src/OverwriteMinesweeperJar.java
+++ app/src/OverwriteMinesweeperJar.java
@@ -11,7 +11,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 import java.util.jar.JarOutputStream;
 class OverwriteMinesweeperJar {
-    /**@param args String originalJarPath, String tempJarPath, String scoresEntryName, String thisClassName
+    /**@param args String originalJarPath, String scoresFile, String scoresEntryName, String thisFile
      * This class is to get compiled, and later copied out of the jar to a new directory and 
      * loaded on shutdown such that it can overwrite original jar with a new one with a new scores file
      * do not add any internal or anonymous classes or it will compile into more than 1 file, and that file will not be copied.
