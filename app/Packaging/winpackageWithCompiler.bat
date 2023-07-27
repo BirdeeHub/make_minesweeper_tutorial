ab8f6f52a155cf708303eb56aa9edbb57e2588ec
BirdeeHub <robindbor@gmail.com>
2023-07-27 04:55:45 -0700

    changed Icon

--- app/Packaging/winpackageWithCompiler.bat
+++ app/Packaging/winpackageWithCompiler.bat
@@ -2,7 +2,7 @@ IF EXIST "..\Minesweeper_1.0.cfg" (move ..\Minesweeper_1.0.cfg ..\..\)
 IF EXIST "..\Minesweeper_2.0.cfg" (move ..\Minesweeper_2.0.cfg ..\..\)
 IF EXIST "..\Minesweeper_3.0.cfg" (move ..\Minesweeper_3.0.cfg ..\..\)
 IF EXIST "..\Minesweeper_4.0.cfg" (move ..\Minesweeper_4.0.cfg ..\..\)
-jlink --compress=2 --add-modules java.desktop,java.management,jdk.compiler,jdk.jartool --output ..\..\jre
+jlink --compress=2 --add-modules java.desktop,jdk.compiler,jdk.jartool --output ..\..\jre
 jpackage --name Minesweeper_2.0 --description "Minesweeper for Windows with compiler and jar tool included" --input ..\..\app --dest ..\..\ --main-jar minesweeper.jar --icon ..\src\MySweep\Icons\Minesweeper.ico --runtime-image ..\..\jre --win-dir-chooser --win-menu --win-shortcut --win-shortcut-prompt --win-per-user-install --app-version 1.0 --vendor Birdee
 del /s /q ..\..\jre\
 rmdir /s /q ..\..\jre\
